f913b6c6e3815a8582840cff1da54f15eeb8d7e2
Orlin Alvarado <oralvarado1@gmail.com>
2021-04-14 21:26:04 -0600

    Aplicacion finalizada

---
+++ lib/screens/calculator_screen.dart
@@ -0,0 +1,41 @@
+import 'package:calculadora/controllers/calculator_controller.dart';
+import 'package:calculadora/widgets/calc_buttons.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'package:calculadora/widgets/math_results.dart';
+class CalculatorScreen extends StatelessWidget {
+  
+  final calculatorCtrl = Get.put( CalculatorController());
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Container(
+            constraints: BoxConstraints(
+              maxWidth: GetPlatform.isWeb ?  320 : 600
+            ),
+            margin: EdgeInsets.symmetric( horizontal: 10 ),
+            child: Column(
+              children: [
+                
+                Expanded(
+                  child: Container(),
+                ),
+
+                MathResults(),
+                CalculatorButtons(),
+                
+                if( GetPlatform.isWeb )
+                  SizedBox(height: 100)
+                
+              ],
+            ),
+          ),
+        ),
+      )
+   );
+  }
+}
